7e659b0a343ff2b365c0c214da6f0e8c9945b95f
Andrew Bell <andrewbell545@gmail.com>
2019-11-11 16:31:09 +0000

    added buttons with the onclick for each modal. Now require to make it IE9 compatible as well as perhaps making the buttons shadow a bit when the modal is open.

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,18 +1,40 @@
 // get the modal box.
 let modalContainer = document.querySelector(".modalContainer");
 let modal = document.querySelector(".modalBox");
+let modal2 = document.querySelector(".modalBox2");
 let body = document.querySelector("body");
-let main = document.getElementById("main");
-let button1 = document.querySelector(".listItem1");
+let button1 = document.querySelector(".modalOne");
+let button2 = document.querySelector(".modalTwo");
+
+let modalArr = [];
+modalArr.push(modal);
+modalArr.push(modal2);
+console.log(modalArr);
 
 modalContainer.style.display = "none";
 
 button1.onclick = () => {
-  modalContainer.style.display = "block"; // not supported by IE9.
+  modalContainer.style.display = "block"; // block supported by IE9.
+  if ((modal2.style.display = "block")) {
+    modal2.style.display = "none";
+    modal.style.display = "block";
+  }
   if (body.classList.contains("backgroundToNormal")) {
     body.classList.remove("backgroundToNormal");
   }
   body.classList.add("backgroundChange"); // not supported by IE9.
+};
+
+button2.onclick = () => {
+  modalContainer.style.display = "block";
+  if ((modal.style.display = "block")) {
+    modal.style.display = "none";
+    modal2.style.display = "block";
+  }
+  if (body.classList.contains("backgroundToNormal")) {
+    body.classList.remove("backgroundToNormal");
+  }
+  body.classList.add("backgroundChange");
 };
 
 window.onclick = event => {
@@ -23,5 +45,3 @@ window.onclick = event => {
     body.classList.add("backgroundToNormal");
   }
 };
-
-loadModal();
