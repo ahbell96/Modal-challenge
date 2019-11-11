5c228ad2ec628562f15b186cbcf0e804a95dfb51
Andrew Bell <andrewbell545@gmail.com>
2019-11-11 16:12:13 +0000

    fixed margin-top onclick. added button so now fires from button rather than from when window loads straight. Now will make another Modal for button two.

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,9 +3,15 @@ let modalContainer = document.querySelector(".modalContainer");
 let modal = document.querySelector(".modalBox");
 let body = document.querySelector("body");
 let main = document.getElementById("main");
+let button1 = document.querySelector(".listItem1");
 
-let loadModal = () => {
-  modalContainer.style.display = "flex"; // not supported by IE9.
+modalContainer.style.display = "none";
+
+button1.onclick = () => {
+  modalContainer.style.display = "block"; // not supported by IE9.
+  if (body.classList.contains("backgroundToNormal")) {
+    body.classList.remove("backgroundToNormal");
+  }
   body.classList.add("backgroundChange"); // not supported by IE9.
 };
 
@@ -14,7 +20,7 @@ window.onclick = event => {
     //modal.classList.toggle(".dispNone");
     modalContainer.style.display = "none";
     body.classList.remove("backgroundChange");
-    body.classList.add("background-to-normal");
+    body.classList.add("backgroundToNormal");
   }
 };
 
