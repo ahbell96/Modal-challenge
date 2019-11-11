69eb66c20467dba329f0928ca13c163fdb576ca1
Andrew Bell <andrewbell545@gmail.com>
2019-11-11 12:32:06 +0000

    made changes to modal. background now changes to light grey. fades in and out when clicked out of.

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,16 +1,20 @@
 // get the modal box.
 let modalContainer = document.querySelector(".modalContainer");
 let modal = document.querySelector(".modalBox");
-let body = document.getElementById("body");
+let body = document.querySelector("body");
+let main = document.getElementById("main");
 
 let loadModal = () => {
-  modalContainer.style.display = "flex";
+  modalContainer.style.display = "flex"; // not supported by IE9.
+  body.classList.add("backgroundChange"); // not supported by IE9.
 };
 
 window.onclick = event => {
   if (event.target == modalContainer) {
     //modal.classList.toggle(".dispNone");
     modalContainer.style.display = "none";
+    body.classList.remove("backgroundChange");
+    body.classList.add("background-to-normal");
   }
 };
 
