52bfed1f04faecfe038a8a6b5eaff654fd3726b8
Andrew Bell <andrewbell545@gmail.com>
2019-11-12 16:53:32 +0000

    replaced build version and moved into the dist/js folder so it can be read by the document

--- dist/js/index.js
+++ dist/js/index.js
@@ -15,36 +15,42 @@ console.log(modalArr);
 
 modalContainer.style.display = "none";
 
-button1.onclick = function () {
+button1.onclick = function() {
   modalContainer.style.display = "block"; // block supported by IE9.
-  if (modal2.style.display = "block") {
+  if ((modal2.style.display = "block")) {
     modal2.style.display = "none";
     modal.style.display = "block";
   }
-  if (body.classList.contains("backgroundToNormal")) {
-    body.classList.remove("backgroundToNormal");
+  if (body.className === "backgroundToNormal") {
+    body.className = "backgroundChange";
+  } else {
+    body.className = "backgroundToNormal";
   }
-  body.classList.add("backgroundChange"); // not supported by IE9.
 };
 
-button2.onclick = function () {
+button2.onclick = function() {
   modalContainer.style.display = "block";
-  if (modal.style.display = "block") {
+  if ((modal.style.display = "block")) {
     modal.style.display = "none";
     modal2.style.display = "block";
   }
-  if (body.classList.contains("backgroundToNormal")) {
-    body.classList.remove("backgroundToNormal");
+  if (body.className === "backgroundToNormal") {
+    body.className = "backgroundChange";
+  } else {
+    body.className = "backgroundToNormal";
   }
-  body.classList.add("backgroundChange");
 };
 
-window.onclick = function (event) {
+window.onclick = function(event) {
   if (event.target == modalContainer) {
-    if (event.target == button1) {}
+    if (event.target == button1) {
+    }
     //modal.classList.toggle(".dispNone");
     modalContainer.style.display = "none";
-    body.classList.remove("backgroundChange");
-    body.classList.add("backgroundToNormal");
+    if (body.className === "backgroundChange") {
+      body.className = "backgroundToNormal";
+    }
+    //body.classList.remove("backgroundChange");
+    //body.classList.add("backgroundToNormal");
   }
 };
