2bd29ea885af5977477fa3ca4730118b06dd389c
Andrew Bell <andrewbell545@gmail.com>
2019-11-12 13:56:44 +0000

    used babel to make es5 downscale to check ie9 workings

--- dist/js/index-es6.js
+++ dist/js/index-es6.js
@@ -1,23 +1,21 @@
-"use strict";
-
 // get the modal box.
-var modalContainer = document.querySelector(".modalContainer");
-var modal = document.querySelector(".modalBox");
-var modal2 = document.querySelector(".modalBox2");
-var body = document.querySelector("body");
-var button1 = document.querySelector(".modalOne");
-var button2 = document.querySelector(".modalTwo");
+let modalContainer = document.querySelector(".modalContainer");
+let modal = document.querySelector(".modalBox");
+let modal2 = document.querySelector(".modalBox2");
+let body = document.querySelector("body");
+let button1 = document.querySelector(".modalOne");
+let button2 = document.querySelector(".modalTwo");
 
-var modalArr = [];
+let modalArr = [];
 modalArr.push(modal);
 modalArr.push(modal2);
 console.log(modalArr);
 
 modalContainer.style.display = "none";
 
-button1.onclick = function () {
+button1.onclick = () => {
   modalContainer.style.display = "block"; // block supported by IE9.
-  if (modal2.style.display = "block") {
+  if ((modal2.style.display = "block")) {
     modal2.style.display = "none";
     modal.style.display = "block";
   }
@@ -27,9 +25,9 @@ button1.onclick = function () {
   body.classList.add("backgroundChange"); // not supported by IE9.
 };
 
-button2.onclick = function () {
+button2.onclick = () => {
   modalContainer.style.display = "block";
-  if (modal.style.display = "block") {
+  if ((modal.style.display = "block")) {
     modal.style.display = "none";
     modal2.style.display = "block";
   }
@@ -39,9 +37,10 @@ button2.onclick = function () {
   body.classList.add("backgroundChange");
 };
 
-window.onclick = function (event) {
+window.onclick = event => {
   if (event.target == modalContainer) {
-    if (event.target == button1) {}
+    if (event.target == button1) {
+    }
     //modal.classList.toggle(".dispNone");
     modalContainer.style.display = "none";
     body.classList.remove("backgroundChange");
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,21 +1,23 @@
-// get the modal box.
-let modalContainer = document.querySelector(".modalContainer");
-let modal = document.querySelector(".modalBox");
-let modal2 = document.querySelector(".modalBox2");
-let body = document.querySelector("body");
-let button1 = document.querySelector(".modalOne");
-let button2 = document.querySelector(".modalTwo");
+"use strict";
 
-let modalArr = [];
+// get the modal box.
+var modalContainer = document.querySelector(".modalContainer");
+var modal = document.querySelector(".modalBox");
+var modal2 = document.querySelector(".modalBox2");
+var body = document.querySelector("body");
+var button1 = document.querySelector(".modalOne");
+var button2 = document.querySelector(".modalTwo");
+
+var modalArr = [];
 modalArr.push(modal);
 modalArr.push(modal2);
 console.log(modalArr);
 
 modalContainer.style.display = "none";
 
-button1.onclick = () => {
+button1.onclick = function () {
   modalContainer.style.display = "block"; // block supported by IE9.
-  if ((modal2.style.display = "block")) {
+  if (modal2.style.display = "block") {
     modal2.style.display = "none";
     modal.style.display = "block";
   }
@@ -25,9 +27,9 @@ button1.onclick = () => {
   body.classList.add("backgroundChange"); // not supported by IE9.
 };
 
-button2.onclick = () => {
+button2.onclick = function () {
   modalContainer.style.display = "block";
-  if ((modal.style.display = "block")) {
+  if (modal.style.display = "block") {
     modal.style.display = "none";
     modal2.style.display = "block";
   }
@@ -37,10 +39,9 @@ button2.onclick = () => {
   body.classList.add("backgroundChange");
 };
 
-window.onclick = event => {
+window.onclick = function (event) {
   if (event.target == modalContainer) {
-    if (event.target == button1) {
-    }
+    if (event.target == button1) {}
     //modal.classList.toggle(".dispNone");
     modalContainer.style.display = "none";
     body.classList.remove("backgroundChange");
