da3651ba14aad8cad93371d437a89d60a42fa0af
Andrew Bell <andrewbell545@gmail.com>
2019-11-12 15:37:45 +0000

    classList removal, changed to className so supports IE9

--- build/js/index.js
+++ build/js/index.js
@@ -15,33 +15,36 @@ console.log(modalArr);
 
 modalContainer.style.display = "none";
 
-button1.onclick = function () {
+button1.onclick = function() {
   modalContainer.style.display = "block"; // block supported by IE9.
-  if (modal2.style.display = "block") {
+  if ((modal2.style.display = "block")) {
     modal2.style.display = "none";
     modal.style.display = "block";
   }
-  if (body.classList.contains("backgroundToNormal")) {
-    body.classList.remove("backgroundToNormal");
+  if (body.className === "backgroundToNormal") {
+    body.className = "backgroundChange";
+  } else {
+    body.className = "backgroundToNormal";
   }
-  body.classList.add("backgroundChange"); // not supported by IE9.
 };
 
-button2.onclick = function () {
+button2.onclick = function() {
   modalContainer.style.display = "block";
-  if (modal.style.display = "block") {
+  if ((modal.style.display = "block")) {
     modal.style.display = "none";
     modal2.style.display = "block";
   }
-  if (body.classList.contains("backgroundToNormal")) {
-    body.classList.remove("backgroundToNormal");
+  if (body.className === "backgroundToNormal") {
+    body.className = "backgroundChange";
+  } else {
+    body.className = "backgroundToNormal";
   }
-  body.classList.add("backgroundChange");
 };
 
-window.onclick = function (event) {
+window.onclick = function(event) {
   if (event.target == modalContainer) {
-    if (event.target == button1) {}
+    if (event.target == button1) {
+    }
     //modal.classList.toggle(".dispNone");
     modalContainer.style.display = "none";
     body.classList.remove("backgroundChange");
