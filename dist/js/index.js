c5491828d4e9f45ea3c97d187e63945c84472782
Andrew Bell <andrewbell545@gmail.com>
2019-11-13 15:27:21 +0000

    added new overlay, looks much better. However is CSS3.

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,21 +2,18 @@
 
 // get the modal box.
 var modalContainer = document.querySelector(".modalContainer");
+var overlay = document.querySelector(".overlay");
 var modal = document.querySelector(".modalBox");
 var modal2 = document.querySelector(".modalBox2");
 var body = document.querySelector("body");
 var button1 = document.querySelector(".modalOne");
 var button2 = document.querySelector(".modalTwo");
 
-var modalArr = [];
-modalArr.push(modal);
-modalArr.push(modal2);
-console.log(modalArr);
-
 modalContainer.style.display = "none";
 
 button1.onclick = function() {
   modalContainer.style.display = "block"; // block supported by IE9.
+  overlay.style.display = "block";
   if ((modal2.style.display = "block")) {
     modal2.style.display = "none";
     modal.style.display = "block";
@@ -30,22 +27,23 @@ button1.onclick = function() {
 
 button2.onclick = function() {
   modalContainer.style.display = "block";
+  overlay.style.display = "block";
   if ((modal.style.display = "block")) {
     modal.style.display = "none";
     modal2.style.display = "block";
   }
   if (body.className === "backgroundToNormal") {
     body.className = "backgroundChange";
+    overlay.style.transition = "ease-in-out 2s";
   } else {
     body.className = "backgroundToNormal";
   }
 };
 
 window.onclick = function(event) {
-  if (event.target == modalContainer) {
-    if (event.target == button1) {
-    }
+  if (event.target == modalContainer || event.target == overlay) {
     //modal.classList.toggle(".dispNone");
+    overlay.style.display = "none";
     modalContainer.style.display = "none";
     if (body.className === "backgroundChange") {
       body.className = "backgroundToNormal";
